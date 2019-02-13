cd411c4807fa3e911e97a4fcc2bddab24f852821
Miles <loganmh94@yahoo.com>
2019-02-13 10:35:29 -0600

    fixed typo and more spacing problems

--- part_1/fix_me.sh
+++ part_1/fix_me.sh
@@ -14,18 +14,17 @@ sub=$((a-b))
 mul=$((a*b))
         echo Multiplication of a and b are $mul
 
-div=$((a/d))
+div=$((a/b))
         echo division of a and b are $div
+
 mod=$((a%b))
         echo Modulus of a and b are $mod
 
       ((++a))
-          echo Increment
-          operator when applied on "a" results into a = $a
+        echo Increment operator when applied on "a" results into a = $a
 
       ((--b))
-          echo Decrement
-          operator when applied on "b" results into b = $b
+          echo Decrement operator when applied on "b" results into b = $b
 
 (
 (--c)
